eb4c56d3a503659904fedaa869315ca5bfaf2730
刘江写代码养你呀 <173024756@qq.com>
2021-10-07 20:01:47 +0800

    test

---
+++ src/com.java.designPattern/SingleResponsibility.java
@@ -0,0 +1,11 @@
+package com.java.designPattern;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: liujiang
+ * @Date: 2021/10/07/19:56
+ * @Description:
+ */
+public class SingleResponsibility {
+}
