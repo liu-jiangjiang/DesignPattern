5090396cb79e188204233612d37f6130fcce3201
刘江写代码养你呀 <173024756@qq.com>
2021-10-26 22:15:32 +0800

    doc

--- src/com/java/designPattern/singleResponsibility/SingleResponsibility3.java
+++ src/com/java/designPattern/singleResponsibility/SingleResponsibility3.java
@@ -6,6 +6,9 @@ package com.java.designPattern.singleResponsibility;
  * @Auther: liujiang
  * @Date: 2021/10/07/20:30
  * @Description: 单一职责原则
+ * 对类来说的，即一个类应该只负责一项职责。如类A负责两个不同职责：职责1，职责2。
+ * 当职责1需求变更而改变A时，可能造成职责2执行错误，所以需要将类A的粒度分解为
+ * A1，A2
  */
 public class SingleResponsibility3 {
     public static void main(String[] args) {
